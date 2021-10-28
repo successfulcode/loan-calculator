4aa812a4616352ae1e279c4ad11d7a5d700002f6
successfulcode <56683358+successfulcode@users.noreply.github.com>
2021-10-29 00:13:27 +0300

    Created reactive form in calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CalculatorComponent } from './calculator/calculator.component';
@@ -12,7 +12,9 @@ import { CalculatorComponent } from './calculator/calculator.component';
     CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    const newLoan = {
+      monthlyIncome: this.calculatorForm.value.income,
+      requestedAmount: this.calculatorForm.value.amount,
+      loanTerm: this.calculatorForm.value.term,
+      children: this.calculatorForm.value.children,
+      coapplicant: this.calculatorForm.value.coapplicant
+    }
+    console.log('onSubmit', newLoan)
   }
 
+  calculatorForm = new FormGroup({
+    income: new FormControl(),
+    children: new FormControl('NONE'),
+    coapplicant: new FormControl('NONE'),
+    amount: new FormControl(),
+    term: new FormControl()
+  });
 }
+
